f1466d2f67163bdd6a2d1129dfbe07e08d07035e
Nash SIng <nashayle.sing@outlook.com>
2019-01-07 11:50:44 +0200

    Added meat to these files

--- TemporalTable.sql
+++ TemporalTable.sql
@@ -2,35 +2,105 @@ use Development
 
 --Temporal Table
 
--- Link - https://www.red-gate.com/simple-talk/sql/sql-training/sql-server-temporal-tables-recipes/
+-- You cannot drop a table that has system versioning on, first have to turn it off
+ALTER TABLE dbo.tb_SystemSetting SET (SYSTEM_VERSIONING = OFF);
+DROP TABLE dbo.tb_SystemSetting
+DROP TABLE dbo.SystemSettingAudit
 
 
-select * from sys.tables
-
+-- Requirements for creating temporal tables (NEW)
 CREATE TABLE dbo.tb_SystemSetting
     (
-    SettingId INT NOT NULL PRIMARY KEY CLUSTERED,
+    SettingId INT NOT NULL PRIMARY KEY CLUSTERED, -- Need a PK
     SettingName VARCHAR(50) NOT NULL,
     IntValue INT NULL,
     StringValue VARCHAR(100) NULL,
-    SysStartTime DATETIME2 GENERATED ALWAYS AS ROW START
-      CONSTRAINT DF_SystemSetting_SysStartTime DEFAULT SYSUTCDATETIME() NOT NULL,
-    SysEndTime DATETIME2 GENERATED ALWAYS AS ROW END
-      CONSTRAINT DF_SystemSetting_SysEndTime 
-  	DEFAULT CONVERT( DATETIME2, '9999-12-31 23:59:59' ) NOT NULL,
-    PERIOD FOR SYSTEM_TIME(SysStartTime, SysEndTime)
+    SysStartTime DATETIME2 GENERATED ALWAYS AS ROW START, --[HIDDEN] --Required to show validity period
+    SysEndTime DATETIME2 GENERATED ALWAYS AS ROW END, --[HIDDEN] -- Required to show validity period
+    PERIOD FOR SYSTEM_TIME(SysStartTime, SysEndTime) -- Required as designation period for columns
     )
-  WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = dbo.SystemSettingAudit));
+  WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = dbo.SystemSettingAudit)); -- Required to set system versioning on as well as a linked history table
+       -- HISTORY_RETENTION_PERIOD = 2 YEARS , this is only available in SQL 2017+
+
 
   -- Show the different ways to select, between, for system time , as of etc
   select * from dbo.tb_SystemSetting
-  Select * from dbo.SystemSettingAudit
+  Select * from dbo.SystemSettingAudit -- There is no PK created as it is not allowed and there is a Clustered index created on SysEndTime, SysStartTime
 
   sp_help 'dbo.tb_SystemSetting'
+  sp_help 'dbo.SystemSettingAudit'
 
+  
+  --Modifying Data
 
   --No data will be inserted into history table
-  insert into dbo.tb_SystemSetting (SettingId,SettingName,IntValue,StringValue) values (1,'Activate Bluetooth',0, Null)
+  insert into dbo.tb_SystemSetting (SettingId, SettingName, IntValue, StringValue) 
+                            values (1,'Activate Bluetooth', 0 , Null);
+
+  select * from dbo.tb_SystemSetting -- SysStartTime is inserted as UTC and EndTime is set to max time
+  Select * from dbo.SystemSettingAudit -- No data inserted here 
 
   --Data will now be inserted into the history table, look at dates
-  update dbo.tb_SystemSetting set IntValue = 1 where SettingId = 1
+  update dbo.tb_SystemSetting set IntValue = 1 where SettingId = 1;
+
+  select * from dbo.tb_SystemSetting -- SysStartTime is updated as UTC and EndTime is set to max time, new value shows up
+  Select * from dbo.SystemSettingAudit -- Old value is now inserted in here, SysStartTime shows the time this was first instered into parent table and SysEndTime shows the time the update occurred , this shows the period in whic this record was valid for
+
+  -- Data will now be deleted from parent table and inserted into history table
+  Delete from dbo.tb_SystemSetting where SettingId = 1;
+
+  select * from dbo.tb_SystemSetting -- Row is removed
+  Select * from dbo.SystemSettingAudit -- Shows the record that was deleted along with the time it was valid for (UTC) 
+
+
+  -- Querying Data (Will automatically query the parent table as well as the history table)
+  -- Use SYSTEM_TIME clause with one of the following subclauses against the table
+  -- 4 ways:
+  
+  -- This shows you what the data looked like at a certain point in time
+  select * from dbo.tb_SystemSetting for system_time as of '2019-01-07 09:09:38.9124921' -- This can be a varaible
+  -- SysStartTime <= @DT and SysEndTime > @DT
+
+
+  -- This shows you what the data looked like from a certain point in time To a certain point in time (Not inclusive)
+   select * from dbo.tb_SystemSetting for system_time from '2019-01-07' to '2019-01-08'  -- This can be a varaible
+   -- SysStartTime < @End and SysEndTime > @Start
+
+   -- This shows you what the data looked like from a certain point in time To a certain point in time (End Inclusive)
+   select * from dbo.tb_SystemSetting for system_time between '2019-01-07' and '2019-01-08'  -- This can be a varaible
+   -- SysStartTime <= @End and SysEndTime > @Start
+
+   -- This shows you what the data looked like from a certain point in time To a certain point in time (Both Inclusive)
+   select * from dbo.tb_SystemSetting for system_time contained in ( '2019-01-07','2019-01-08')  -- This can be a varaible
+   -- SysStartTime >= @Start and SysEndTime <= @End
+
+   -- To get all versions
+   SELECT *
+   FROM dbo.tb_SystemSetting  FOR SYSTEM_TIME ALL;
+
+
+   --Note
+   -- If you start a transaction and affect multiple rows and that tran runs for 5 mins , the time will be taken as transaction start time
+
+   Begin
+
+   Begin Tran
+
+     insert into dbo.tb_SystemSetting (SettingId, SettingName, IntValue, StringValue) 
+                            values (2,'Activate Brake', 0 , Null);
+
+							waitfor delay '00:00:10'
+
+							     insert into dbo.tb_SystemSetting (SettingId, SettingName, IntValue, StringValue) 
+                            values (3,'Activate Indicator', 0 , Null);
+
+
+   commit tran
+
+   End
+
+   --Query and check the start times will be the same
+   select * from dbo.tb_SystemSetting
+
+
+   -- You can also add system versioning to existing tables(gets more complicated if tables have data in them)
---
+++ TemporalTableTheory.sql
@@ -0,0 +1,47 @@
+/*
+-- Link - https://www.red-gate.com/simple-talk/sql/sql-training/sql-server-temporal-tables-recipes/
+
+Temporal, or system-versioned, tables were introduced as a database feature in SQL Server 2016. This gives us a type of table that can provide information about the data that was stored at any specified time rather than just the data that is current. ANSI SQL 2011 first specified a temporal table as a database feature and this is now supported in SQL Server.
+
+The most common business uses for temporal tables are:
+
+Slowly changing dimensions.
+The temporal tables provide a simpler way to querying data that is current for a specified period of time, such as time slicing data, that well-known problem on Data Warehousing databases.
+
+Data Auditing. 
+The temporal tables provide an audit trail to determine when data was modified in the “parent” table. This helps to meet the requirements of regulatory compliance and to do data forensics when needed by tracking and auditing data changes over time.
+
+Repairing or recovering record level corruptions.
+Establishing a way of ‘undoing’ a data change on a table’s row without downtime in case a record is accidentally deleted or updated. Therefore, the previous version of the data can be retrieved from the history table and inserted back into the ‘parent’ table. – This helps when someone (or because of some application errors) accidentally deletes data and you want to revert to it or recover it.
+
+Reproducing financial reports, invoices and statements 
+with the correct data for the date of issue of the document. Temporal tables allow you to query data as it was at a particular point in time to examine the state of the data as it was then.
+
+Analyzing trends by understanding how the data changes over time with the ongoing business activity, and to calculate trends in the way that data changes over time.
+
+
+How can this benefit us ?
+In the dark days before SQL Server 2016 was introduced, the data-logging mechanism had to be established explicitly in a trigger. We still use triggers to do this for us.
+
+The temporal tables feature of SQL Server 2016 can dramatically simplify the logging mechanism. This article provides step-by-step instructions on how to accomplish system-versioned tables.
+
+To migrate a table into the temporal table, a temporal table option can be set on an existing table. 
+To create a new temporal table, you just need to set the temporal table option to ON (for example, SYSTEM_VERSIONING = ON). When the temporal table option is enabled, SQL Server 2016 generates the “historical” table automatically, and internally maintains both parent and historical tables, one for storing the actual data and the other for the historical data. The temporal table’s SYSTEM_TIME period columns (for example SysStartTime and SysEndTime) enables the mechanism to query data for a different time slice more efficiently. The updated or deleted data moves into the “historical” table, whilst the “parent” table keeps the latest row version for updated records.
+
+
+What is the catch?
+The most important considerations, restrictions and limitations of Temporal Tables are:
+
+In order to relate records between the Temporal Table and the history table, you must have a primary key in the Temporal Table. However, the history table cannot have a primary key.
+The datetime2 datatype must be set for the SYSTEM_TIME period columns (for example SysStartTime and SysEndTime).
+When you create a history table, you must always specify both the schema and table name of the temporal table in the history table.
+The PAGE compression is the default setting for the history table.
+The Temporal Tables support blob data types, (nvarchar(max), varchar(max), varbinary(max), ntext, text, and image), that could affect the storage costs and have performance issues.
+Both temporal and history tables must be created in the same database. You cannot use a Linked Server to provide the Temporal Tables.
+You cannot use constraints, primary key, foreign keys or column constraints for the history tables.
+You cannot reference Temporal Tables in indexed views that have queries that use the FOR SYSTEM_TIME clause
+The SYSTEM_TIME period columns cannot be directly referenced in INSERT and UPDATE statements.
+You cannot use TRUNCATE TABLE while SYSTEM_VERSIONING is ON.
+You are not allowed to directly modify the data in a history table.
+
+*/
